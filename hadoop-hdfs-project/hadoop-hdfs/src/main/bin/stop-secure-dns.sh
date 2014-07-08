0f08c379d5b002951f409125353b30e31eb26557
Raja Aluri <raja@cmbasics.com>
2014-07-08 11:45:10 -0700

    BUG-18619 Fixing the default DEFAULT_LIBEXEC_DIR

--- hadoop-hdfs-project/hadoop-hdfs/src/main/bin/stop-secure-dns.sh
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/bin/stop-secure-dns.sh
@@ -23,6 +23,11 @@ bin=`dirname "${BASH_SOURCE-$0}"`
 bin=`cd "$bin"; pwd`
 
 DEFAULT_LIBEXEC_DIR="$bin"/../libexec
+
+if [ -n "$HADOOP_HOME" ]; then
+  DEFAULT_LIBEXEC_DIR="$HADOOP_HOME"/libexec
+fi
+
 HADOOP_LIBEXEC_DIR=${HADOOP_LIBEXEC_DIR:-$DEFAULT_LIBEXEC_DIR}
 . $HADOOP_LIBEXEC_DIR/hdfs-config.sh
 
