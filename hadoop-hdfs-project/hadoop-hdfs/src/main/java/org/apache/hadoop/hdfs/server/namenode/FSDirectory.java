179f25e290e81be12dee4d0016b2f36d8b232dd8
Arpit Agarwal <aagarwal@hortonworks.com>
2014-10-01 10:36:28 -0700

    Revert "BUG-23663. [HDFS-7155] Bugfix in createLocatedFileStatus caused by bad merge. (Arpit Agarwal)"
    This reverts commit 9b73196bea108088a47756e298df8d6562fbad15.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSDirectory.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSDirectory.java
@@ -2442,7 +2442,6 @@ public class FSDirectory implements Closeable {
       final INodeFile fileNode = node.asFile();
       size = fileNode.computeFileSize(snapshot);
       replication = fileNode.getFileReplication(snapshot);
-      blocksize = fileNode.getPreferredBlockSize();
       isLazyPersist = fileNode.getLazyPersistFlag();
 
       final boolean inSnapshot = snapshot != Snapshot.CURRENT_STATE_ID; 
