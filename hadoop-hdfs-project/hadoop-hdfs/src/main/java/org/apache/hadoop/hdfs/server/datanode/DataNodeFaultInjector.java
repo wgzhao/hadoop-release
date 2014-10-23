39b2c72dcebd633e63c7adaf5c3e02394db89496
Andrew Wang <wang@apache.org>
2014-10-23 12:53:01 -0700

    HDFS-7222. Expose DataNode network errors as a metric. (Charles Lamb via wang)
    (cherry picked from commit 86cad007d7d6366b293bb9a073814889081c8662)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNodeFaultInjector.java

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNodeFaultInjector.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNodeFaultInjector.java
@@ -39,4 +39,5 @@ public class DataNodeFaultInjector {
   public void getHdfsBlocksMetadata() {}
 
   public void sendShortCircuitShmResponse() throws IOException {}
+  public void writeBlockAfterFlush() throws IOException {}
 }
