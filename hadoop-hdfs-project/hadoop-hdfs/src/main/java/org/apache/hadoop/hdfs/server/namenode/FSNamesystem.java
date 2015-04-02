48bf107b834a3836e6cfe4d752e8d161810783e0
Haohui Mai <wheat9@apache.org>
2015-04-02 11:38:02 -0700

    HDFS-8035. Move checkBlocksProperlyReplicated() in FSNamesystem to BlockManager. Contributed by Haohui Mai.
    (cherry picked from commit 6e36dbf03a6e2769a24a79f8fe2cc7c00d9525c8)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -3054,49 +3054,20 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
    * all blocks, otherwise check only penultimate block.
    */
   boolean checkFileProgress(String src, INodeFile v, boolean checkall) {
+    assert hasReadLock();
     if (checkall) {
-      // check all blocks of the file.
-      for (BlockInfoContiguous block: v.getBlocks()) {
-        if (!isCompleteBlock(src, block, blockManager.minReplication)) {
-          return false;
-        }
-      }
+      return blockManager.checkBlocksProperlyReplicated(src, v
+          .getBlocks());
     } else {
       // check the penultimate block of this file
       BlockInfoContiguous b = v.getPenultimateBlock();
-      if (b != null
-          && !isCompleteBlock(src, b, blockManager.minReplication)) {
-        return false;
-      }
+      return b == null ||
+          blockManager.checkBlocksProperlyReplicated(
+              src, new BlockInfoContiguous[] { b });
     }
-    return true;
   }
 
-  private static boolean isCompleteBlock(String src, BlockInfoContiguous b, int minRepl) {
-    if (!b.isComplete()) {
-      final BlockInfoContiguousUnderConstruction uc = (BlockInfoContiguousUnderConstruction)b;
-      final int numNodes = b.numNodes();
-      LOG.info("BLOCK* " + b + " is not COMPLETE (ucState = "
-          + uc.getBlockUCState() + ", replication# = " + numNodes
-          + (numNodes < minRepl? " < ": " >= ")
-          + " minimum = " + minRepl + ") in file " + src);
-      return false;
-    }
-    return true;
-  }
-
-  ////////////////////////////////////////////////////////////////
-  // Here's how to handle block-copy failure during client write:
-  // -- As usual, the client's write should result in a streaming
-  // backup write to a k-machine sequence.
-  // -- If one of the backup machines fails, no worries.  Fail silently.
-  // -- Before client is allowed to close and finalize file, make sure
-  // that the blocks are backed up.  Namenode may have to issue specific backup
-  // commands to make up for earlier datanode failures.  Once all copies
-  // are made, edit namespace and return to client.
-  ////////////////////////////////////////////////////////////////
-
-  /** 
+  /**
    * Change the indicated filename. 
    * @deprecated Use {@link #renameTo(String, String, boolean,
    * Options.Rename...)} instead.
