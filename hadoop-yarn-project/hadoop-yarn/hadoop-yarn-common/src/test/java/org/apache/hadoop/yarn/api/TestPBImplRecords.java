27933bac0e9acbd89499f7260329e69fe5cb2c18
Jian He <jianhe@apache.org>
2015-04-10 10:40:31 -0700

    BUG-46456. YARN-3348. Add a 'yarn top' tool to help understand cluster usage. Contributed by Varun Vasudev
    (cherry picked from commit d3daf9665c1cb4af86e31260960120f0d02d68c1)
    
     Conflicts:
    	hadoop-yarn-project/CHANGES.txt
    	hadoop-yarn-project/hadoop-yarn/bin/yarn
    	hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/api/TestPBImplRecords.java

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/api/TestPBImplRecords.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/api/TestPBImplRecords.java
@@ -216,7 +216,8 @@ public class TestPBImplRecords {
     // it is recursive(has sub queues)
     typeValueCache.put(QueueInfo.class, QueueInfo.newInstance("root", 1.0f,
         1.0f, 0.1f, null, null, QueueState.RUNNING, ImmutableSet.of("x", "y"),
-        "x && y"));
+        "x && y", null));
+    generateByNewInstance(QueueStatistics.class);
     generateByNewInstance(QueueUserACLInfo.class);
     generateByNewInstance(YarnClusterMetrics.class);
     // for reservation system
