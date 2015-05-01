0a5ed82dec8add914e7a6b980b2271f20f258c93
Gera Shegalov <gera@apache.org>
2015-05-01 15:44:36 -0700

    HADOOP-11491. HarFs incorrectly declared as requiring an authority. (Brahma Reddy Battula via gera)
    (cherry picked from commit f343f8657e2b01773a32c2c7d960dc368954b42e)
    (cherry picked from commit 58970d69de8a1662e4548cd6d4ca460dd70562f8)

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/TestHarFileSystemBasics.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/TestHarFileSystemBasics.java
@@ -399,4 +399,11 @@ public class TestHarFileSystemBasics {
     }
   }
 
+  @Test
+  public void testHarFsWithoutAuthority() throws Exception {
+    final URI uri = harFileSystem.getUri();
+    Assert.assertNull("har uri authority not null: " + uri, uri.getAuthority());
+    FileContext.getFileContext(uri, conf);
+  }
+
 }
