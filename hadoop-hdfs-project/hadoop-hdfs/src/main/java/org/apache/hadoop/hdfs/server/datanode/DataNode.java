a5de4692082d6d4cc41dc54c05e3f03ace12ddbc
Colin Patrick Mccabe <cmccabe@cloudera.com>
2015-06-02 11:40:37 -0700

    HDFS-8486. DN startup may cause severe data loss (Daryn Sharp via Colin P.  McCabe)
    (cherry picked from commit 03fb5c642589dec4e663479771d0ae1782038b63)
    (cherry picked from commit 17e369511dd18d6e25e10ddb0cd9511f7ec8f469)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNode.java
@@ -1378,9 +1378,9 @@ public class DataNode extends ReconfigurableBase
     // failures.
     checkDiskError();
 
-    initDirectoryScanner(conf);
     data.addBlockPool(nsInfo.getBlockPoolID(), conf);
     blockScanner.enableBlockPoolId(bpos.getBlockPoolId());
+    initDirectoryScanner(conf);
   }
 
   BPOfferService[] getAllBpOs() {
