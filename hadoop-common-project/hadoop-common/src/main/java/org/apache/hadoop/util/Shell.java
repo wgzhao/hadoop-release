c67b4e627e7518e23b32158e8f58e75c67b15fa5
Gera Shegalov <gera@apache.org>
2015-06-30 14:44:48 -0700

    YARN-3768. ArrayIndexOutOfBoundsException with empty environment variables. (Zhihai Xu via gera)
    (cherry picked from commit 6f2a41e37d0b36cdafcfff75125165f212c612a6)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/Shell.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/Shell.java
@@ -232,10 +232,12 @@ abstract public class Shell {
     }
   }
 
+  public static final String ENV_NAME_REGEX = "[A-Za-z_][A-Za-z0-9_]*";
   /** Return a regular expression string that match environment variables */
   public static String getEnvironmentVariableRegex() {
-    return (WINDOWS) ? "%([A-Za-z_][A-Za-z0-9_]*?)%" :
-      "\\$([A-Za-z_][A-Za-z0-9_]*)";
+    return (WINDOWS)
+        ? "%(" + ENV_NAME_REGEX + "?)%"
+        : "\\$(" + ENV_NAME_REGEX + ")";
   }
   
   /**
