d3997ae018aa8383528d07b8fca003523a8b1b17
Wangda Tan <wangda@apache.org>
2015-07-24 17:01:06 -0700

    YARN-3487. CapacityScheduler scheduler lock obtained unnecessarily when calling getQueue. Contributed by Jason Lowe.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacityScheduler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacityScheduler.java
@@ -611,7 +611,7 @@ public class CapacityScheduler extends
     return queue;
   }
 
-  public synchronized CSQueue getQueue(String queueName) {
+  public CSQueue getQueue(String queueName) {
     if (queueName == null) {
       return null;
     }
@@ -909,11 +909,7 @@ public class CapacityScheduler extends
       boolean includeChildQueues, boolean recursive) 
   throws IOException {
     CSQueue queue = null;
-
-    synchronized (this) {
-      queue = this.queues.get(queueName); 
-    }
-
+    queue = this.queues.get(queueName);
     if (queue == null) {
       throw new IOException("Unknown queue: " + queueName);
     }
