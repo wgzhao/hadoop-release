9eca313ff1fa3a0a1dd479a57a7732e82be2f2e0
Anubhav Dhoot <adhoot@apache.org>
2015-09-27 20:52:38 -0700

    YARN-4204. ConcurrentModificationException in FairSchedulerQueueInfo. (adhoot)
    (cherry picked from commit fb2e525c0775ccf218c8980676e9fb4005a406a6)
    
    Conflicts:
    	hadoop-yarn-project/CHANGES.txt
    
    (cherry picked from commit 4d8b99423e55eb206ef68b637b24c1a9bbd6a1e5)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FSLeafQueue.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FSLeafQueue.java
@@ -69,7 +69,8 @@ public class FSLeafQueue extends FSQueue {
   private Resource amResourceUsage;
 
   private final ActiveUsersManager activeUsersManager;
-  
+  public static final List<FSQueue> EMPTY_LIST = Collections.emptyList();
+
   public FSLeafQueue(String name, FairScheduler scheduler,
       FSParentQueue parent) {
     super(name, scheduler, parent);
@@ -377,7 +378,7 @@ public class FSLeafQueue extends FSQueue {
 
   @Override
   public List<FSQueue> getChildQueues() {
-    return new ArrayList<FSQueue>(1);
+    return EMPTY_LIST;
   }
   
   @Override
