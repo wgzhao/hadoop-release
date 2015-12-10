b1e93fa17dab9f2554113f0df952ecb1e5067405
Junping Du <junping_du@apache.org>
2015-12-10 02:44:30 -0800

    YARN-3623. Add a config to indicate the Timeline Service version. Contributed by Xuan Gong. (cherry picked from commit f910e4f639dc311fcb257bfcb869b1aa8b2c0643)
    (cherry picked from commit 0b5e76e9ff14811ad2617e632854f21d82558e5e)
    
    (cherry picked from commit 005ef46e4eda81700a0d2bff039216118719126a)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/conf/YarnConfiguration.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/conf/YarnConfiguration.java
@@ -1429,9 +1429,9 @@ public class YarnConfiguration extends Configuration {
   public static final String TIMELINE_SERVICE_PREFIX =
       YARN_PREFIX + "timeline-service.";
 
-  /** Timeline client settings */
-  public static final String TIMELINE_SERVICE_CLIENT_PREFIX =
-      TIMELINE_SERVICE_PREFIX + "client.";
+  public static final String TIMELINE_SERVICE_VERSION = TIMELINE_SERVICE_PREFIX
+      + "version";
+  public static final float DEFAULT_TIMELINE_SERVICE_VERSION = 1.0f;
 
   /**
    * Comma seperated list of names for UIs hosted in the timeline server
