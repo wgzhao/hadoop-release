be4c21d61d8b4ceb2691d476647ee5919aa8f0c3
Wangda Tan <wangda@apache.org>
2015-12-14 11:24:30 -0800

    YARN-4418. AM Resource Limit per partition can be updated to ResourceUsage as well. (Sunil G via wangda)
    (cherry picked from commit 07b0fb996a32020678bd2ce482b672f0434651f0)
    
    Conflicts:
    	hadoop-yarn-project/CHANGES.txt

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/LeafQueue.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/LeafQueue.java
@@ -586,6 +586,7 @@ public class LeafQueue extends AbstractCSQueue {
         minimumAllocation);
 
     metrics.setAMResouceLimit(amResouceLimit);
+    queueUsage.setAMLimit(nodePartition, amResouceLimit);
     return amResouceLimit;
   }
 
