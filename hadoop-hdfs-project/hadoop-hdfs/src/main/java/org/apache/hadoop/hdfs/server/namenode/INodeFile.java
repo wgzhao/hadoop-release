8b6a70139036a89cd8963ea34201cb38e88f58b9
Yongjun Zhang <yzhang@cloudera.com>
2016-02-01 11:23:44 -0800

    BUG-51889. HDFS-9406. FSImage may get corrupted after deleting snapshot. (yzhang)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/INodeFile.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/INodeFile.java
@@ -525,7 +525,7 @@ public class INodeFile extends INodeWithAdditionalFields
         blk.setBlockCollection(null);
       }
     }
-    setBlocks(null);
+    setBlocks(BlockInfo.EMPTY_ARRAY);
     clear();
     removedINodes.add(this);
     FileWithSnapshotFeature sf = getFileWithSnapshotFeature();
