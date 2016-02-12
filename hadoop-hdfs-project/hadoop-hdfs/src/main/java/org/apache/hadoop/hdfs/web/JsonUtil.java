49d5b9254018611a6d52a4ed2e923b8e2ee1d4d5
Akira Ajisaka <aajisaka@apache.org>
2016-02-13 01:57:24 +0900

    HDFS-9768. Reuse ObjectMapper instance in HDFS to improve the performance. Contributed by Lin Yiqun.
    (cherry picked from commit e6a7044b8530afded8f8e86ff309dd0e4d39238a)
    (cherry picked from commit a714d04f5d8eaba34a24ee9454ceaec68ab00a15)
    (cherry picked from commit df23f9c49d384063a980b81fcb290e090f6e0526)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/tools/offlineImageViewer/FSImageLoader.java
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/JsonUtil.java
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/oauth2/ConfRefreshTokenBasedAccessTokenProvider.java
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/oauth2/CredentialBasedAccessTokenProvider.java

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/JsonUtil.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/JsonUtil.java
@@ -806,4 +806,9 @@ public class JsonUtil {
     String path = (String) json.get("Path");
     return path;
   }
+
+  public static String toJsonString(Object obj) throws IOException {
+    return MAPPER.writeValueAsString(obj);
+  }
+
 }
