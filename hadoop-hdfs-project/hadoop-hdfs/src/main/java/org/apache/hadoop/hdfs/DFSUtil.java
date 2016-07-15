6843a92d35b0f7c2d7897ed0791a4703f033761c
Arpit Agarwal <arp@apache.org>
2016-07-14 22:06:29 -0700

    HDFS-8887. Expose storage type and storage ID in BlockLocation. (Andrew Wang)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSUtil.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSUtil.java
@@ -534,6 +534,8 @@ public class DFSUtil {
       }
       blkLocations[idx] = new BlockLocation(xferAddrs, hosts, cachedHosts,
                                             racks,
+                                            blk.getStorageIDs(),
+                                            blk.getStorageTypes(),
                                             blk.getStartOffset(),
                                             blk.getBlockSize(),
                                             blk.isCorrupt());
