c1cce48cf698cd3a06b7bb76ebec6a82456b410c
Varun Vasudev <vvasudev@apache.org>
2016-07-20 13:06:31 +0530

    BUG-73054. YARN-5309. Fix SSLFactory truststore reloader thread leak in TimelineClientImpl. Contributed by Weiwei Yang.
    (cherry picked from commit 1b8641a3c3b16d798ecd5956ba92b8911cacab95)
    (cherry picked from commit 93a05a9564f0d0dd670a4b0a2fa902e63648e451)
    
    Change-Id: Iccc008dd55d73977626777e4d84dccdeb82743b0

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/api/impl/TimelineClientImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/api/impl/TimelineClientImpl.java
@@ -142,6 +142,7 @@ public class TimelineClientImpl extends TimelineClient {
   private URI resURI;
   private UserGroupInformation authUgi;
   private String doAsUser;
+  private SSLFactory sslFactory;
   private Path activePath = null;
   private FileSystem fs = null;
   private Set<String> summaryEntityTypes;
@@ -327,7 +328,7 @@ public class TimelineClientImpl extends TimelineClient {
     }
     ClientConfig cc = new DefaultClientConfig();
     cc.getClasses().add(YarnJacksonJaxbJsonProvider.class);
-    connConfigurator = newConnConfigurator(conf);
+    connConfigurator = initConnConfigurator(conf);
     if (UserGroupInformation.isSecurityEnabled()) {
       authenticator = new KerberosDelegationTokenAuthenticator();
     } else {
@@ -427,6 +428,9 @@ public class TimelineClientImpl extends TimelineClient {
 
   @Override
   protected void serviceStop() throws Exception {
+    if (this.sslFactory != null) {
+      this.sslFactory.destroy();
+    }
     if (this.logFDsCache != null) {
       this.logFDsCache.close();
     }
@@ -626,9 +630,9 @@ public class TimelineClientImpl extends TimelineClient {
 
   }
 
-  private static ConnectionConfigurator newConnConfigurator(Configuration conf) {
+  private ConnectionConfigurator initConnConfigurator(Configuration conf) {
     try {
-      return newSslConnConfigurator(DEFAULT_SOCKET_TIMEOUT, conf);
+      return initSslConnConfigurator(DEFAULT_SOCKET_TIMEOUT, conf);
     } catch (Exception e) {
       LOG.debug("Cannot load customized ssl related configuration. " +
           "Fallback to system-generic settings.", e);
@@ -646,16 +650,15 @@ public class TimelineClientImpl extends TimelineClient {
     }
   };
 
-  private static ConnectionConfigurator newSslConnConfigurator(final int timeout,
+  private ConnectionConfigurator initSslConnConfigurator(final int timeout,
       Configuration conf) throws IOException, GeneralSecurityException {
-    final SSLFactory factory;
     final SSLSocketFactory sf;
     final HostnameVerifier hv;
 
-    factory = new SSLFactory(SSLFactory.Mode.CLIENT, conf);
-    factory.init();
-    sf = factory.createSSLSocketFactory();
-    hv = factory.getHostnameVerifier();
+    sslFactory = new SSLFactory(SSLFactory.Mode.CLIENT, conf);
+    sslFactory.init();
+    sf = sslFactory.createSSLSocketFactory();
+    hv = sslFactory.getHostnameVerifier();
 
     return new ConnectionConfigurator() {
       @Override
