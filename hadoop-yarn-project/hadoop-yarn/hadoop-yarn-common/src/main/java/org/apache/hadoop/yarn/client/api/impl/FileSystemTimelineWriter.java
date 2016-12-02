55121d561d4fb555e79f7bfad830c0cf0f45333d
Jason Lowe <jlowe@apache.org>
2016-12-02 16:54:15 +0000

    [YARN-5915] ATS 1.5 FileSystemTimelineWriter causes flush() to be called after every event write.
    Change-Id: Id2f9ba7e24837a19f649ed09d5eb44b7ce299ac9
    (cherry picked from commit 040adfda08e586aeb66d113a1252de089cc8b3a8)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/api/impl/FileSystemTimelineWriter.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/api/impl/FileSystemTimelineWriter.java
@@ -283,6 +283,7 @@ public class FileSystemTimelineWriter extends TimelineWriter{
     mapper.setAnnotationIntrospector(new JaxbAnnotationIntrospector());
     mapper.setSerializationInclusion(Inclusion.NON_NULL);
     mapper.configure(Feature.CLOSE_CLOSEABLE, false);
+    mapper.configure(Feature.FLUSH_AFTER_WRITE_VALUE, false);
     return mapper;
   }
 
@@ -363,6 +364,7 @@ public class FileSystemTimelineWriter extends TimelineWriter{
 
     public void flush() throws IOException {
       if (stream != null) {
+        jsonGenerator.flush();
         stream.hflush();
       }
     }
