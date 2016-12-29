8fb6fca6bfc3bd810d4440b2556ac48ed6202fd5
Gergely Novak <gnovak@hortonworks.com>
2016-12-29 10:37:12 +0100

    BUG-74492. HADOOP-11312. Fix unit tests to not use uppercase key names.
    Change-Id: I542b2aad747c67e73781b6f7038606aa0de23d42

--- hadoop-common-project/hadoop-kms/src/test/java/org/apache/hadoop/crypto/key/kms/server/TestKMS.java
+++ hadoop-common-project/hadoop-kms/src/test/java/org/apache/hadoop/crypto/key/kms/server/TestKMS.java
@@ -1560,8 +1560,10 @@ public class TestKMS {
     conf.set("hadoop.kms.authentication.kerberos.principal", "HTTP/localhost");
     conf.set("hadoop.kms.authentication.kerberos.name.rules", "DEFAULT");
 
-    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kA.ALL", "*");
-    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kD.ALL", "*");
+    final String keyA = "key_a";
+    final String keyD = "key_d";
+    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + keyA + ".ALL", "*");
+    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + keyD + ".ALL", "*");
 
     writeConf(testDir, conf);
 
@@ -1577,7 +1579,7 @@ public class TestKMS {
 
         try {
           KeyProvider kp = new KMSClientProvider(uri, conf);
-          kp.createKey("kA", new KeyProvider.Options(conf));
+          kp.createKey(keyA, new KeyProvider.Options(conf));
         } catch (IOException ex) {
           System.out.println(ex.getMessage());
         }
@@ -1598,7 +1600,7 @@ public class TestKMS {
 
         try {
           KeyProvider kp = new KMSClientProvider(uri, conf);
-          kp.createKey("kA", new KeyProvider.Options(conf));
+          kp.createKey(keyA, new KeyProvider.Options(conf));
         } catch (IOException ex) {
           System.out.println(ex.getMessage());
         }
@@ -1607,7 +1609,7 @@ public class TestKMS {
           @Override
           public Void run() throws Exception {
             KeyProvider kp = new KMSClientProvider(uri, conf);
-            kp.createKey("kD", new KeyProvider.Options(conf));
+            kp.createKey(keyD, new KeyProvider.Options(conf));
             return null;
           }
         });
@@ -1642,10 +1644,10 @@ public class TestKMS {
     conf.set("hadoop.kms.authentication.kerberos.name.rules", "DEFAULT");
     conf.set("hadoop.kms.proxyuser.client.users", "foo,bar");
     conf.set("hadoop.kms.proxyuser.client.hosts", "*");
-    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kAA.ALL", "client");
-    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kBB.ALL", "foo");
-    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kCC.ALL", "foo1");
-    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kDD.ALL", "bar");
+    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kaa.ALL", "client");
+    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kbb.ALL", "foo");
+    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kcc.ALL", "foo1");
+    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kdd.ALL", "bar");
 
     writeConf(testDir, conf);
 
@@ -1670,7 +1672,7 @@ public class TestKMS {
           @Override
           public Void run() throws Exception {
             final KeyProvider kp = new KMSClientProvider(uri, conf);
-            kp.createKey("kAA", new KeyProvider.Options(conf));
+            kp.createKey("kaa", new KeyProvider.Options(conf));
 
             // authorized proxyuser
             UserGroupInformation fooUgi =
@@ -1678,7 +1680,7 @@ public class TestKMS {
             fooUgi.doAs(new PrivilegedExceptionAction<Void>() {
               @Override
               public Void run() throws Exception {
-                Assert.assertNotNull(kp.createKey("kBB",
+                Assert.assertNotNull(kp.createKey("kbb",
                     new KeyProvider.Options(conf)));
                 return null;
               }
@@ -1691,7 +1693,7 @@ public class TestKMS {
               @Override
               public Void run() throws Exception {
                 try {
-                  kp.createKey("kCC", new KeyProvider.Options(conf));
+                  kp.createKey("kcc", new KeyProvider.Options(conf));
                   Assert.fail();
                 } catch (AuthorizationException ex) {
                   // OK
@@ -1708,7 +1710,7 @@ public class TestKMS {
             barUgi.doAs(new PrivilegedExceptionAction<Void>() {
               @Override
               public Void run() throws Exception {
-                Assert.assertNotNull(kp.createKey("kDD",
+                Assert.assertNotNull(kp.createKey("kdd",
                     new KeyProvider.Options(conf)));
                 return null;
               }
@@ -1748,9 +1750,9 @@ public class TestKMS {
     conf.set("hadoop.security.kms.client.timeout", "300");
     conf.set("hadoop.kms.proxyuser.client.users", "foo,bar");
     conf.set("hadoop.kms.proxyuser.client.hosts", "*");
-    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kAA.ALL", "foo");
-    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kBB.ALL", "foo1");
-    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kCC.ALL", "bar");
+    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kaa.ALL", "foo");
+    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kbb.ALL", "foo1");
+    conf.set(KeyAuthorizationKeyProvider.KEY_ACL + "kcc.ALL", "bar");
 
     writeConf(testDir, conf);
 
@@ -1782,7 +1784,7 @@ public class TestKMS {
               @Override
               public Void run() throws Exception {
                 KeyProvider kp = new KMSClientProvider(uri, conf);
-                Assert.assertNotNull(kp.createKey("kAA",
+                Assert.assertNotNull(kp.createKey("kaa",
                     new KeyProvider.Options(conf)));
                 return null;
               }
@@ -1796,7 +1798,7 @@ public class TestKMS {
               public Void run() throws Exception {
                 try {
                   KeyProvider kp = new KMSClientProvider(uri, conf);
-                  kp.createKey("kBB", new KeyProvider.Options(conf));
+                  kp.createKey("kbb", new KeyProvider.Options(conf));
                   Assert.fail();
                 } catch (Exception ex) {
                   Assert.assertTrue(ex.getMessage(), ex.getMessage().contains("Forbidden"));
@@ -1812,7 +1814,7 @@ public class TestKMS {
               @Override
               public Void run() throws Exception {
                 KeyProvider kp = new KMSClientProvider(uri, conf);
-                Assert.assertNotNull(kp.createKey("kCC",
+                Assert.assertNotNull(kp.createKey("kcc",
                     new KeyProvider.Options(conf)));
                 return null;
               }
