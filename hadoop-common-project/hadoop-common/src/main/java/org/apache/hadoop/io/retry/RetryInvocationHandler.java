apache/hadoop

HADOOP-13552. RetryInvocationHandler logs all remote exceptions. Contributed by Jason Lowe (cherry picked from commit 92d8f371553b88e5b3a9d3354e93f75d60d81368)

hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/retry/RetryInvocationHandler.java
@@ -351,9 +351,11 @@ public class RetryInvocationHandler<T> implements RpcInvocationHandler {
     if (retryInfo.isFail()) {
       // fail.
       if (retryInfo.action.reason != null) {
-        LOG.warn("Exception while invoking call #" + callId + " "
-            + proxyDescriptor.getProxyInfo().getString(method.getName())
-            + ". Not retrying because " + retryInfo.action.reason, e);
+        if (LOG.isDebugEnabled()) {
+          LOG.debug("Exception while invoking call #" + callId + " "
+              + proxyDescriptor.getProxyInfo().getString(method.getName())
+              + ". Not retrying because " + retryInfo.action.reason, e);
+        }
       }
       throw e;
     }

wgzhao/hadoop-release

Conflicts:
	hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/retry/RetryInvocationHandler.java
Change-Id: Iec65073d4bcf5be284809220a0ed3ad765069252

hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/retry/RetryInvocationHandler.java
@@ -214,9 +214,11 @@ public class RetryInvocationHandler<T> implements RpcInvocationHandler {
     if (retryInfo.fail != null) {
       // fail.
       if (retryInfo.fail.reason != null) {
-        LOG.warn("Exception while invoking "
-            + proxyDescriptor.getProxyInfo().getString(method.getName())
-            + ". Not retrying because " + retryInfo.fail.reason, ex);
+        if (LOG.isDebugEnabled()) {
+          LOG.debug("Exception while invoking "
+              + proxyDescriptor.getProxyInfo().getString(method.getName())
+              + ". Not retrying because " + retryInfo.fail.reason, ex);
+        }
       }
       throw ex;
     }