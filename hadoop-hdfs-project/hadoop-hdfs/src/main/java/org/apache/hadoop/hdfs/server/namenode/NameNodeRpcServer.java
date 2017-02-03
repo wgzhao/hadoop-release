c76764e43e01c7fb18f778db3d500f25d182bebc
Hanisha Koneru <hkoneru@hortonworks.com>
2017-02-02 16:09:09 -0800

    BUG-68077. HDFS-10301. BlockReport retransmissions may lead to storages falsely being declared zombie if storage report processing happens out of order
    Change-Id: I6ce57014fd9ebc2d830fb8bbdf2135d4f6de3ab6

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNodeRpcServer.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNodeRpcServer.java
@@ -1463,7 +1463,7 @@ public class NameNodeRpcServer implements NamenodeProtocols {
         @Override
         public Boolean call() throws IOException {
           return bm.processReport(nodeReg, reports[index].getStorage(),
-              blocks, context, (index == reports.length - 1));
+              blocks, context);
         }
       });
       metrics.incrStorageBlockReportOps();
