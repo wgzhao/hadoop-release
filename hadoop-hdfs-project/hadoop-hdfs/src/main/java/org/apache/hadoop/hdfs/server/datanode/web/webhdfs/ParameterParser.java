b3c7ffb2d7c37e013708e2924b2a9d1b41607a78
Jitendra Pandey <jitendra@apache.org>
2017-10-02 16:28:08 -0700

    BUG-89277. HADOOP-14910 Upgrade netty-all jar to latest 4.0.x.Final. Contributed by Vinayakumar B.
    Change-Id: Iefd4d1b21d81cc9ee32bc724237653c25ac2ab58

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/web/webhdfs/ParameterParser.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/web/webhdfs/ParameterParser.java
@@ -57,8 +57,7 @@ class ParameterParser {
   private final Map<String, List<String>> params;
 
   ParameterParser(QueryStringDecoder decoder, Configuration conf) {
-    this.path = decodeComponent(decoder.path().substring
-        (WEBHDFS_PREFIX_LENGTH), Charsets.UTF_8);
+    this.path = decoder.path().substring(WEBHDFS_PREFIX_LENGTH);
     this.params = decoder.parameters();
     this.conf = conf;
   }
