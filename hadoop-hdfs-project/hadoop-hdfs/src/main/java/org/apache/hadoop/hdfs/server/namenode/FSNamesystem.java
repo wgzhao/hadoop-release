37479e5f5dec577e213c08a68e770a7a1d59f638
Gergely Novak <gnovak@hortonworks.com>
2017-10-19 16:47:42 +0200

    BUG-90173. HDFS-6763. Initialize file system-wide quota once on transitioning to active.
    (cherry picked from commit 72bb07ffb6)
    
    Change-Id: I5ce3977f8782657492850016c22fac348bf7055c

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -1135,6 +1135,8 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
         getFSImage().editLog.openForWrite(getEffectiveLayoutVersion());
       }
 
+      // Initialize the quota.
+      dir.updateCountForQuota();
       // Enable quota checks.
       dir.enableQuotaChecks();
       if (haEnabled) {
