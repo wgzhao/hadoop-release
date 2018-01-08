3e1dd7ef76e61fec7363927985b4cccf6e5443d6
Rohith Sharma K S <rohithsharmaks@apache.org>
2018-01-08 16:10:30 +0530

    BUG-96394. Both RM are in standby in secure cluster. YARN-6061. Add an UncaughtExceptionHandler for critical threads in RM. (Yufei Gu via kasha) YARN-6061. Addendum. Remove extraneous change. YARN-3742. YARN RM will shut down if ZKClient creation times out. (Daniel Templeton via kasha)
    Change-Id: I1e748bd722f557a1554c3073155d24ce2a0648c8

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/AdminService.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/AdminService.java
@@ -315,13 +315,12 @@ public class AdminService extends CompositeService implements
       // call all refresh*s for active RM to get the updated configurations.
       refreshAll();
     } catch (Exception e) {
-      LOG.error("RefreshAll failed so firing fatal event", e);
       rm.getRMContext()
           .getDispatcher()
           .getEventHandler()
           .handle(
               new RMFatalEvent(RMFatalEventType.TRANSITION_TO_ACTIVE_FAILED,
-                  e));
+                  e, "failure to refresh configuration settings"));
       throw new ServiceFailedException(
           "Error on refreshAll during transition to Active", e);
     }
