5d6b630fa42762bcecf476ded9c24ac774997d50
Sean Mackrory <mackrorysd@apache.org>
2018-03-08 11:23:23 -0700

    HADOOP-15299. Bump Jackson 2 version to Jackson 2.9.x.
    Change-Id: I8cc8c3655f03372f026af190211e019b6016d298

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/api/impl/FileSystemTimelineWriter.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/api/impl/FileSystemTimelineWriter.java
@@ -22,6 +22,7 @@ import java.io.Closeable;
 import java.io.FileNotFoundException;
 import java.io.Flushable;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -382,7 +383,8 @@ public class FileSystemTimelineWriter extends TimelineWriter{
 
     protected void prepareForWrite() throws IOException{
       this.stream = createLogFileStream(fs, logPath);
-      this.jsonGenerator = new JsonFactory().createGenerator(stream);
+      this.jsonGenerator = new JsonFactory().createGenerator(
+          (OutputStream)stream);
       this.jsonGenerator.setPrettyPrinter(new MinimalPrettyPrinter("\n"));
       this.lastModifiedTime = Time.monotonicNow();
     }
