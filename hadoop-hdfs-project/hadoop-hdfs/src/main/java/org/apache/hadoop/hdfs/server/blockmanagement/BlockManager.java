8ee7dcee4c65595529cffc7c12f222b3363da258
Surendra Singh Lilhore <surendralilhore@apache.org>
2019-09-13 00:17:13 +0530

    CDPD-4418. HDFS-14754. Erasure Coding : The number of Under-Replicated Blocks never reduced. Contributed by  hemanthboyina.
    Change-Id: Ie25355ec3ed8e8cf3bcabd7090169f9db8810241

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
@@ -3326,8 +3326,7 @@ public class BlockManager implements BlockStatsMXBean {
   private boolean shouldProcessExtraRedundancy(NumberReplicas num,
       int expectedNum) {
     final int numCurrent = num.liveReplicas();
-    return numCurrent > expectedNum ||
-        (numCurrent == expectedNum && num.redundantInternalBlocks() > 0);
+    return numCurrent > expectedNum || num.redundantInternalBlocks() > 0;
   }
 
   /**
